24870fd5d5116c36c4ecad069c36756321894699
Domonkos Zsolt <domonkoszsolt@MacBook-Air.local>
2016-12-09 09:37:01 +0100

    Upload

---
+++ mysql_backup_FULL_daily.sh
@@ -0,0 +1,21 @@
+!/bin/bash
+rm -rfv  /opt/backup/DAILY-MYSQL/*
+
+root@backend:/home/domonz# cat /etc/bacula/scripts/mysql_backup_FULL_daily.sh
+#! /bin/bash
+
+DATE=$(date +"%F")
+BACKUP_DIR="/opt/backup/DAILY-MYSQL"
+MYSQL_USER="root"
+MYSQL=/usr/bin/mysql
+MYSQL_PASSWORD=""
+MYSQLDUMP=/usr/bin/mysqldump
+
+
+
+databases=`$MYSQL --host=127.0.0.1 --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+
+for db in $databases; do
+ $MYSQLDUMP  --host=127.0.0.1 --opt --routines --triggers --events --skip-lock-tables --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | gzip > $BACKUP_DIR/"$db.`date +%m%d%Y%H%M%S`.gz"
+done
+ chmod -R 777 $BACKUP_DIR
---
+++ mysql_del_FULL_daily.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -rfv  /opt/backup/DAILY-MYSQL/*
